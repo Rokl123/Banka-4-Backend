e63e24c5de03e4e1d0bb39c264249822cc601c7b
Arsenije <apetrovic10322rn@raf.rs>
2025-03-11 17:08:20 +0100

    Add a logic for creating the loan, waiting for other service to finish it up
    # Conflicts:
    #	user-service/src/main/java/rs/banka4/user_service/exceptions/account/AccountNotActive.java
    #	user-service/src/main/java/rs/banka4/user_service/repositories/LoanRepository.java
    #	user-service/src/main/java/rs/banka4/user_service/service/abstraction/LoanService.java
    #	user-service/src/main/java/rs/banka4/user_service/service/impl/LoanServiceImpl.java
    
    # Conflicts:
    #	user-service/src/main/java/rs/banka4/user_service/repositories/LoanRepository.java
    #	user-service/src/main/java/rs/banka4/user_service/service/impl/LoanServiceImpl.java
    
    # Conflicts:
    #	user-service/src/main/java/rs/banka4/user_service/service/impl/LoanServiceImpl.java

--- user-service/src/main/java/rs/banka4/user_service/domain/loan/mapper/LoanMapper.java
+++ user-service/src/main/java/rs/banka4/user_service/domain/loan/mapper/LoanMapper.java
@@ -1,9 +1,11 @@
 package rs.banka4.user_service.domain.loan.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 import rs.banka4.user_service.domain.loan.db.Loan;
+import rs.banka4.user_service.domain.loan.dtos.LoanApplicationDto;
 import rs.banka4.user_service.domain.loan.dtos.LoanInformationDto;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -12,5 +14,8 @@ public interface LoanMapper {
     LoanMapper INSTANCE = Mappers.getMapper(LoanMapper.class);
 
     LoanInformationDto toDto(Loan loan);
+
+    @Mapping(source = "loanType",target = "type")
+    Loan toEntity(LoanApplicationDto loanApplicationDto);
 
 }
--- user-service/src/main/java/rs/banka4/user_service/service/impl/LoanServiceImpl.java
+++ user-service/src/main/java/rs/banka4/user_service/service/impl/LoanServiceImpl.java
@@ -4,44 +4,70 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import rs.banka4.user_service.domain.loan.db.LoanInstallment;
 import rs.banka4.user_service.domain.loan.db.LoanStatus;
 import rs.banka4.user_service.domain.loan.db.Loan;
-import rs.banka4.user_service.domain.loan.db.PaymentStatus;
 import rs.banka4.user_service.domain.loan.dtos.LoanApplicationDto;
 import rs.banka4.user_service.domain.loan.dtos.LoanFilterDto;
 import rs.banka4.user_service.domain.loan.dtos.LoanInformationDto;
-import rs.banka4.user_service.repositories.LoanInstallmentRepository;
 import rs.banka4.user_service.repositories.LoanRepository;
-import rs.banka4.user_service.utils.loans.LoanRateUtil;
+import rs.banka4.user_service.service.abstraction.AccountService;
 import rs.banka4.user_service.exceptions.jwt.Unauthorized;
 import rs.banka4.user_service.exceptions.loan.InvalidLoanStatus;
 import rs.banka4.user_service.exceptions.loan.LoanNotFound;
 import rs.banka4.user_service.domain.loan.mapper.LoanMapper;
 import rs.banka4.user_service.domain.loan.specification.LoanSpecification;
-import rs.banka4.user_service.repositories.LoanRepository;
 import rs.banka4.user_service.service.abstraction.LoanService;
 import rs.banka4.user_service.utils.JwtUtil;
-
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
+@Primary
 public class LoanServiceImpl implements LoanService {
-    private final LoanRateUtil loanRateUtil;
+
+    private final AccountService accountService;
+
     private final LoanRepository loanRepository;
-    private final LoanInstallmentRepository loanInstallmentRepository;
+
     private final JwtUtil jwtUtil;
 
-    @Override
-    public void createLoanApplication(LoanApplicationDto loanApplicationDto) {
 
+    @Transactional
+    @Override
+    public void createLoanApplication(LoanApplicationDto loanApplicationDto, String auth) {
+
+//        String email = jwtUtil.extractUsername(auth);
+//
+//        Optional<Client> client = clientService.getClientByEmail(email);
+//
+//        if(client.isEmpty())
+//            throw new ClientNotFound(email);
+
+        Loan newLoan = LoanMapper.INSTANCE.toEntity(loanApplicationDto);
+
+        newLoan.setStatus(LoanStatus.PROCESSING);
+
+        newLoan.setNextInstallmentDate(LocalDate.now().plusMonths(1));
+
+        newLoan.setRemainingDebt(loanApplicationDto.amount());
+
+        connectAccountToLoan(loanApplicationDto,newLoan);
+
+        generateAccountNumber(newLoan);
+
+        // TODO: Calculate the monthly installment for the loan
+
+        loanRepository.save(newLoan);
     }
+
+
 
     @Override
     public ResponseEntity<Page<LoanInformationDto>> getAllLoans(PageRequest pageRequest, LoanFilterDto filterDto) {
@@ -73,7 +99,7 @@ public class LoanServiceImpl implements LoanService {
         if (loan.isEmpty())
             throw new LoanNotFound();
 
-        if (!loan.get().getStatus().equals(LoanStatus.PROCESSING))
+        if(!loan.get().getStatus().equals(LoanStatus.PROCESSING))
             throw new InvalidLoanStatus(loan.get().getStatus().name());
 
         loan.get().setNextInstallmentDate(LocalDate.now().plusMonths(1));
@@ -82,18 +108,6 @@ public class LoanServiceImpl implements LoanService {
         loan.get().setAgreementDate(LocalDate.now());
 
         loanRepository.save(loan.get());
-
-        var loneInstallment = new LoanInstallment();
-        var interest = loan.get().getInterestRate() == null ? null :
-                loan.get().getInterestRate().getFixedRate();
-
-        loneInstallment.setLoan(loan.get());
-        loneInstallment.setInstallmentAmount(loan.get().getMonthlyInstallment());
-        loneInstallment.setExpectedDueDate(loan.get().getDueDate());
-        loneInstallment.setInterestRateAmount(interest);
-        loneInstallment.setPaymentStatus(PaymentStatus.UNPAID);
-
-        loanInstallmentRepository.save(loneInstallment);
     }
 
     @Transactional
@@ -108,11 +122,41 @@ public class LoanServiceImpl implements LoanService {
         if (loan.isEmpty())
             throw new LoanNotFound();
 
-        if (!loan.get().getStatus().equals(LoanStatus.PROCESSING))
+        if(!loan.get().getStatus().equals(LoanStatus.PROCESSING))
             throw new InvalidLoanStatus(loan.get().getStatus().name());
 
         loan.get().setStatus(LoanStatus.REJECTED);
 
         loanRepository.save(loan.get());
+
+    }
+
+    private void connectAccountToLoan(LoanApplicationDto loanApplicationDto, Loan newLoan) {
+
+    }
+
+    private void generateAccountNumber(Loan newLoan){
+        String comb;
+
+        LocalDate dayOfLoanRequest = LocalDate.now();
+
+        if(dayOfLoanRequest.getMonthValue() < 10)
+             comb = dayOfLoanRequest.getYear() + "0" + dayOfLoanRequest.getMonthValue();
+        else
+            comb = dayOfLoanRequest.getYear() + "" + dayOfLoanRequest.getMonthValue();
+
+        while(true){
+            try{
+                long random = ThreadLocalRandom.current().nextLong(0,100000);
+
+                String loanNumber = String.format("%06d",Integer.parseInt(comb) + random);
+
+                newLoan.setLoanNumber(Long.valueOf(loanNumber));
+
+                break;
+            }catch (DataIntegrityViolationException e){
+                System.out.println("Loan with this loan number already exists!");
+            }
+        }
     }
 }
